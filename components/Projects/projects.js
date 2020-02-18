5d0dfc2eba6c84f425514d424a402012defde7e6
Landon Turner <lturner@LTurner-MBP.lan1>
2020-02-17 19:33:46 -0500

    added some functions and built a few components for the home page

---
+++ components/Projects/projects.js
@@ -0,0 +1,8 @@
+function ProjectsHome(){
+    const linkPage = document.createElement('a');
+    linkPage.setAttribute('href', 'projects.html');
+    linkPage.textContent = 'More Here';
+    console.log(linkPage)
+}
+
+function ProjectsMain(){}
