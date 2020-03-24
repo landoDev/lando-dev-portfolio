368b48fceb8d1c77fdc7f0dba11ffd26a42a5fbb
Landon Turner <lturner@LTurner-MBP.lan1>
2020-03-24 11:51:22 -0400

    added react hook form dependency, structured contact form component with empty inputs

--- my-portfolio/src/components/ContactForm.js
+++ my-portfolio/src/components/ContactForm.js
@@ -1,1 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+export const ContactForm = () => {
+    const { register, handleSubmit, watch, errors } = useForm();
+
+    return(
+        <form>
+            <input></input>
+            <input></input>
+            <select>
+                <option></option>
+                <option></option>
+                <option></option>
+            </select>
+            <textarea></textarea>
+        </form>
+    )
+}
