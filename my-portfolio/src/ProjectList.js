18f4bc292615d31587e761b241ab8f7572510993
Landon Turner <lturner@teamworks.com>
2020-06-10 19:08:43 -0400

    map through projects with frontend file

--- my-portfolio/src/ProjectList.js
+++ my-portfolio/src/ProjectList.js
@@ -1,8 +1,21 @@
 export const projects = [
     {
-        name: '',
-        description: '',
-        role: null,
-        link: ''
+        name: 'Weeb Watch: Top Ranked Anime Suggestions',
+        description: 'A quick web app for calling a list of Anime shows and their basic information',
+        role: 'sole-developer',
+        link: 'https://weeb-watch-lj5w55t3e.now.sh/'
+    },
+    {
+        name: 'NASA Photo of the Day',
+        description: 'A web app for a taste of space',
+        role: 'sole-developer',
+        link: 'https://lando-nasa-page.netlify.com/'
+    },
+    {
+        name: 'Merch Dropper',
+        description: 'A hassle free online merch shop. Merch Dropper is a platform to upload designs and create products for a drop-shipping online store. Perfect for small business, fundraising and content-creators',
+        role: 'Full Stack Web Dev',
+        link: 'merchdropper.store'
     }
+
 ]
--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 import { HomeContext } from '../contexts/HomeContext';
 import { ProjectsDiv } from '../styles/index';
+import { projects } from '../ProjectList';
 
 
 
@@ -12,11 +13,16 @@ const Projects = props =>{
         <ProjectsDiv>
             <h2>Projects</h2>
             <div className='live-projects'>
-                {/* MAP THROUGH WHITELIST PROJECTS INSTEAD OF THESE HARD CODED LINKS */}
                 <div className='project-links'>
-                    <a href='https://weeb-watch-lj5w55t3e.now.sh/'>Weeb Watch: Top Ranked Anime Suggestions</a>
-                    <a href='https://landos-git-legion-nlyv30g98.now.sh/'>Lando's Github Legion</a>
-                    <a href='https://lando-nasa-page.netlify.com/'>NASA Photo of the Day</a>
+                    {projects.map(project=>{
+                        return(
+                            <div>
+                                <h2>{project.name}</h2>
+                                <p>{project.description}</p>
+                                <a href={project.link}>{project.link}</a>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </ProjectsDiv>
