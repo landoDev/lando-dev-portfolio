d9d8117a1ae3f78a726892891a490e2ad5d14a7a
Landon Turner <lturner@teamworks.com>
2020-06-12 10:28:07 -0400

    uninstall canvas, looking for way to use iframes effectively

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -1,13 +1,16 @@
 import React, { useContext } from 'react';
 import axios from 'axios';
+import html2canvas from 'html2canvas';
 import Paper from '@material-ui/core/Paper';
 import { HomeContext } from '../contexts/HomeContext';
 import { ProjectsDiv, MappedProjects } from '../styles/index';
 import { projects } from '../ProjectList';
 
 const getPreview = link => {
+    // I'm going to leave this as a possible way to get a preview 
     axios.get(`http://api.linkpreview.net/?key=1d9f3a79b51e7d1179eb873a1d74f538&q=${link}`)
     .then(res =>{
+        {/* https://medium.com/@jamesfuthey/simulating-the-creation-of-website-thumbnail-screenshots-using-iframes-7145269891db */}
         console.log('link preview', res.image)
         return res.image
     }
@@ -28,14 +31,12 @@ const Projects = props =>{
                         // const thumbnail = getPreview(project.link)
                         return(
                             <Paper className='project' elevation={3}>
-                                <div key={project.name}>
+                                <div id={project.name}>
                                     <h2>{project.name}</h2>
                                     <p>{project.description}</p>
+                                    {/* https://medium.com/@jamesfuthey/simulating-the-creation-of-website-thumbnail-screenshots-using-iframes-7145269891db */}
                                     {/* { thumbnail ? <img src={thumbnail} alt='link preview' />
                                     : <span>Preview Unavailable</span> } */}
-                                    <div className='preview'>
-                                        <iframe src={project.link} width = "500px" height = "500px" /> 
-                                    </div>
                                     <a href={project.link}>{project.link}</a>
                                 </div>
                             </Paper>
